f59f3a6ace54b3b9bc42196e0d1ad295123676d2
Lluis Pitarch <lluispitarchripolles@gmail.com>
2020-08-01 10:05:21 +0200

    installing service workers and manifest PWA running

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext, lazy, Suspense } from 'react'
 import { Context } from './Context'
 
 // Styles
 import { GlobalStyles } from './styles/GlobalStyles'
 
 // Pages
-import Home from './pages/Home'
-import NotRegisteredUser from './pages/NotRegisteredUser'
-import User from './pages/User'
-import Favs from './pages/Favs'
-import NotFound from './pages/NotFound'
+const Home = React.lazy(() => import('./pages/Home'))
+const NotRegisteredUser = React.lazy(() => import('./pages/NotRegisteredUser'))
+const User = React.lazy(() => import('./pages/User'))
+const NotFound = React.lazy(() => import('./pages/NotFound'))
+const Favs = React.lazy(() => import('./pages/Favs'))
 
 // Components
 import Logo from './components/Logo'
@@ -22,7 +22,7 @@ import { Router, Redirect } from '@reach/router'
 const App = () => {
   const { isAuth } = useContext(Context)
   return (
-    <>
+    <Suspense fallback={<div />}>
       <GlobalStyles />
       <Logo />
       <Router>
@@ -37,7 +37,7 @@ const App = () => {
         <User path="/user" />
       </Router>
       <NavBar />
-    </>
+    </Suspense>
   )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+path = require('path')
+const workboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   output: {
@@ -8,6 +11,40 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de ftoos de mascotas',
+      shortname: 'Petgram ðŸ¶',
+      description:
+        'Con Petgram puedes encontrar fotos de animales domÃ©sticos my fÃ¡cilmente',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
+    }),
+    new workboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            'https://res.cloudinary.com| images.unsplash.com'
+          ),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images',
+          },
+        },
+        {
+          urlPattern: new RegExp('https://pixtagram-server.vercel.app/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api',
+          },
+        },
+      ],
     }),
   ],
   module: {
